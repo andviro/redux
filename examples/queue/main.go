35304dcaeb2c198070fac71f1dee1c8d5e3aa091
Andrew Rodionoff <andviro@gmail.com>
2018-04-02 17:05:11 +0300

    WIP

--- examples/queue/main.go
+++ examples/queue/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/andviro/redux"
 	"github.com/andviro/redux/middleware"
@@ -11,63 +10,97 @@ import (
 type event struct {
 	ID      int
 	Payload string
+	done    chan struct{}
+}
+
+func (e *event) Wait() {
+	<-e.done
 }
 
 type ack int
 
 type queue struct {
-	id int
-	redux.Store
+	id      int
+	current *event
+	buf     []event
 }
 
-func (q *queue) enqueue(prev redux.State, a redux.Action) redux.State {
-	prevState := prev.([]event)
-	switch t := a.(type) {
-	case event:
-		newState := make([]event, len(prevState)+1)
-		copy(newState, prevState)
-		t.ID = q.id
-		q.id++
-		newState[len(prevState)] = t
-		return newState
-	case ack:
-		var newState []event
-		for _, evt := range prevState {
-			if evt.ID == int(t) {
-				continue
-			}
-			newState = append(newState, evt)
+func (q queue) Enqueue(e event) queue {
+	buf := make([]event, len(q.buf)+1)
+	copy(buf, q.buf)
+	e.ID = q.id
+	q.id++
+	buf[len(q.buf)] = e
+	q.buf = buf
+	q.current = &e
+	return q
+}
+
+func (q queue) Dequeue(id int) queue {
+	var buf []event
+	for _, evt := range q.buf {
+		if evt.ID == id {
+			close(evt.done)
+			continue
 		}
-		return newState
+		buf = append(buf, evt)
 	}
-	return prevState
+	q.buf = buf
+	q.current = nil
+	return q
 }
 
-func Ack(id int) redux.Thunk {
+func Event(payload string) redux.Thunk {
 	return func(dispatch redux.Dispatcher) redux.Action {
-		go dispatch(ack(id))
-		return id
+		res := event{Payload: payload, done: make(chan struct{})}
+		go dispatch(res)
+		return res
 	}
 }
 
-func newQueue() *queue {
-	res := new(queue)
-	var events []event
-	res.Store = redux.New(res.enqueue, events, middleware.Thunk)
-	return res
+func (e *event) Ack() redux.Thunk {
+	return func(dispatch redux.Dispatcher) redux.Action {
+		ack := ack(e.ID)
+		go dispatch(ack)
+		return ack
+	}
+}
+
+func newQueue() redux.Store {
+	return redux.New(func(prev redux.State, a redux.Action) redux.State {
+		q := prev.(queue)
+		switch t := a.(type) {
+		case event:
+			return q.Enqueue(t)
+		case ack:
+			return q.Dequeue(int(t))
+		}
+		return q
+	}, queue{}, middleware.Thunk)
 }
 
 func main() {
 	q := newQueue()
+	n := 0
 	q.Subscribe(func() {
-		evts := q.GetState().([]event)
-		fmt.Println(evts)
-		if len(evts) > 0 {
-			q.Dispatch(Ack(evts[0].ID))
+		evts := q.GetState().(queue)
+		if evts.current != nil {
+			fmt.Println(evts.current.ID)
+			q.Dispatch(evts.current.Ack())
 		}
+		//         fmt.Println(evts.buf)
+		//         //         x, _ := strconv.Atoi(evts.pending.Payload)
+		//         //         n += x
+		//         if len(evts.buf) > 0 {
+		//             q.Dispatch(evts.buf[0].Ack())
+		//         }
 	})
+	var evts []event
 	for i := 0; i < 100; i++ {
-		go q.Dispatch(event{Payload: fmt.Sprint(i)})
+		q.Dispatch(Event(fmt.Sprint(i)))
 	}
-	time.Sleep(10 * time.Second)
+	for _, e := range evts {
+		e.Wait()
+	}
+	fmt.Println(n)
 }
