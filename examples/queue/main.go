a8811badf57203f2c9ddcca7d7550f65d5126ac6
Andrew Rodionoff <andviro@gmail.com>
2018-04-04 17:02:19 +0300

    WIP

--- examples/queue/main.go
+++ examples/queue/main.go
@@ -1,106 +1,58 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/andviro/redux"
 	"github.com/andviro/redux/middleware"
 )
 
 type event struct {
-	ID      int
-	Payload string
-	done    chan struct{}
-}
-
-func (e *event) Wait() {
-	<-e.done
+	id      int
+	payload string
 }
 
 type ack int
 
 type queue struct {
-	id      int
-	current *event
-	buf     []event
+	id  int
+	buf []event
 }
 
-func (q queue) Enqueue(e event) queue {
+func (q queue) Enqueue(payload string) queue {
 	buf := make([]event, len(q.buf)+1)
 	copy(buf, q.buf)
-	e.ID = q.id
 	q.id++
+	e := event{q.id, payload}
 	buf[len(q.buf)] = e
 	q.buf = buf
-	q.current = &e
 	return q
 }
 
 func (q queue) Dequeue(id int) queue {
 	var buf []event
 	for _, evt := range q.buf {
-		if evt.ID == id {
-			close(evt.done)
+		if evt.id == id {
 			continue
 		}
 		buf = append(buf, evt)
 	}
 	q.buf = buf
-	q.current = nil
 	return q
 }
 
-func Event(payload string) redux.Thunk {
-	return func(dispatch redux.Dispatcher) redux.Action {
-		res := event{Payload: payload, done: make(chan struct{})}
-		go dispatch(res)
-		return res
-	}
-}
-
-func (e *event) Ack() redux.Thunk {
-	return func(dispatch redux.Dispatcher) redux.Action {
-		ack := ack(e.ID)
-		go dispatch(ack)
-		return ack
-	}
-}
-
-func newQueue() redux.Store {
+func newQ() redux.Store {
 	return redux.New(func(prev redux.State, a redux.Action) redux.State {
 		q := prev.(queue)
 		switch t := a.(type) {
-		case event:
+		case string:
 			return q.Enqueue(t)
-		case ack:
-			return q.Dequeue(int(t))
+		case int:
+			return q.Dequeue(t)
 		}
 		return q
 	}, queue{}, middleware.Thunk)
 }
 
 func main() {
-	q := newQueue()
-	n := 0
-	q.Subscribe(func() {
-		evts := q.GetState().(queue)
-		if evts.current != nil {
-			fmt.Println(evts.current.ID)
-			q.Dispatch(evts.current.Ack())
-		}
-		//         fmt.Println(evts.buf)
-		//         //         x, _ := strconv.Atoi(evts.pending.Payload)
-		//         //         n += x
-		//         if len(evts.buf) > 0 {
-		//             q.Dispatch(evts.buf[0].Ack())
-		//         }
-	})
-	var evts []event
-	for i := 0; i < 100; i++ {
-		q.Dispatch(Event(fmt.Sprint(i)))
-	}
-	for _, e := range evts {
-		e.Wait()
-	}
-	fmt.Println(n)
+	q := newQ()
+	q.Dispatch("hello")
 }
