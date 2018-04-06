09572b677096cceb2e0e26e69065182f6d022cb9
Andrew Rodionoff <andviro@gmail.com>
2018-04-06 10:53:55 +0300

    streamlined example

--- examples/queue/main.go
+++ examples/queue/main.go
@@ -2,132 +2,89 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 
 	"github.com/andviro/redux"
 	"github.com/andviro/redux/middleware"
 )
 
-type event struct {
-	id      int
-	payload string
-}
+type event string
 
-type ack int
+type sink func(event)
 
-type sink func(string)
+type drop struct{}
 
 type queue struct {
-	id    int
 	buf   []event
 	sinks []sink
-	l     *sync.RWMutex
-}
-
-type take chan event
-
-func (q queue) enqueue(payload string) queue {
-	buf := make([]event, len(q.buf)+1)
-	copy(buf, q.buf)
-	q.id++
-	e := event{q.id, payload}
-	buf[len(q.buf)] = e
-	q.buf = buf
-	return q
-}
-
-func (q queue) dequeue(id int) queue {
-	var buf []event
-	for _, evt := range q.buf {
-		if evt.id == id {
-			continue
-		}
-		buf = append(buf, evt)
-	}
-	q.buf = buf
-	return q
 }
 
 // Emit sends event to sink or buffer
-func Emit(event string) redux.Thunk {
+func Emit(e event) redux.Thunk {
 	return func(dispatch redux.Dispatcher, getState func() redux.State) redux.Action {
+		dispatch(e)
 		q := getState().(queue)
-		if len(q.sinks) == 0 {
-			return dispatch(event)
-		}
-		q.l.RLock()
-		defer q.l.RUnlock()
 		for _, sink := range q.sinks {
-			sink(event)
+			sink(e)
 		}
-		return event
+		return e
 	}
 }
 
-// Sink subscribes function to event
-func Sink(sink sink) redux.Thunk {
+// Sink subscribes function to event and flushes buffer
+func Sink(s sink) redux.Thunk {
 	return func(dispatch redux.Dispatcher, getState func() redux.State) redux.Action {
-		dispatch(sink)
+		dispatch(s)
 		q := getState().(queue)
-		q.l.Lock()
-		defer q.l.Unlock()
-		for _, ev := range q.buf {
-			dispatch(ev.id)
-			for _, sink := range q.sinks {
-				sink(ev.payload)
+		if len(q.sinks) == 1 {
+			for _, evt := range q.buf {
+				s(evt)
 			}
+			dispatch(drop{})
 		}
-		return sink
+		return s
 	}
-}
-
-func (q queue) addSink(t sink) queue {
-	sinks := make([]sink, len(q.sinks)+1)
-	copy(sinks, q.sinks)
-	sinks[len(q.sinks)] = t
-	q.sinks = sinks
-	return q
-}
-
-func newQ() queue {
-	return queue{l: new(sync.RWMutex)}
 }
 
 // NewQ creates queue based on redux.Store
 func NewQ() redux.Store {
-	res := redux.New(func(prev redux.State, a redux.Action) redux.State {
+	res := redux.New(func(prev redux.State, action redux.Action) redux.State {
 		q := prev.(queue)
-		switch t := a.(type) {
+		switch t := action.(type) {
 		case sink:
-			q = q.addSink(t)
-		case string:
-			q = q.enqueue(t)
-		case int:
-			q = q.dequeue(t)
+			q.sinks = append(q.sinks, t)
+		case event:
+			if len(q.sinks) == 0 {
+				q.buf = append(q.buf, t)
+			}
+		case drop:
+			q.buf = nil
 		}
 		return q
-	}, newQ(), middleware.Thunk)
+	}, queue{}, middleware.Thunk)
 	return res
 }
 
 func main() {
 	q := NewQ()
-	q.Dispatch(Emit("hello"))
-	q.Dispatch(Emit("world"))
-	q.Dispatch(Emit("i"))
-	q.Dispatch(Sink(func(s string) {
+	go q.Dispatch(Emit("hello"))
+	go q.Dispatch(Emit("world"))
+	go q.Dispatch(Emit("i"))
+	go q.Dispatch(Sink(func(s event) {
 		fmt.Println("***", s)
 	}))
-	q.Dispatch(Emit("am"))
-	q.Dispatch(Sink(func(s string) {
+	go q.Dispatch(Emit("am"))
+	go q.Dispatch(Sink(func(s event) {
 		fmt.Println("===", s)
 	}))
-	q.Dispatch(Emit("buffered"))
-	q.Dispatch(Emit("queue"))
-	q.Dispatch(Sink(func(s string) {
+	go q.Dispatch(Emit("buffered"))
+	go q.Dispatch(Emit("event"))
+	go q.Dispatch(Emit("queue"))
+	go q.Dispatch(Sink(func(s event) {
 		fmt.Println("---", s)
 	}))
-	q.Dispatch(Emit("with"))
-	q.Dispatch(Emit("multiple"))
-	q.Dispatch(Emit("subscribers"))
+	go q.Dispatch(Emit("with"))
+	go q.Dispatch(Emit("multiple"))
+	go q.Dispatch(Emit("subscribers"))
+	time.Sleep(1 * time.Second)
 }
